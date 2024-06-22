7672547ac7e1370becae9c07a19689d08b822f8c
Jeanaraga <jeanjesuspedrobook@gmail.com>
2024-06-22 19:17:58 -0300

    update aba comentario

--- src/js/home.js
+++ src/js/home.js
@@ -1,30 +1,36 @@
-const btnsLike = document.querySelectorAll('.btn-like');
+const botoesLike = document.querySelectorAll('.btn-like'); // Seleciona todos os botões com classe "btn-like"
 
-btnsLike.forEach(botao => {
-    botao.addEventListener('click', function(e){
-       let botaoClicado = e.target;
+botoesLike.forEach(botao => { // Percorre cada botão "like"
+  botao.addEventListener('click', function(event) {
+    const iconeLike = this.querySelector('.icon-like'); // Seleciona o elemento "icon-like" dentro do botão
+    
+    if (iconeLike.textContent === 'sentiment_neutral') { // Verifica o estado atual do ícone
+      // Curtida
+      iconeLike.textContent = 'mood'; // Muda o ícone para "favoritado"
+      iconeLike.style.color = 'yellow';
+      this.title = "Descurtir"; // Muda o título do botão para "Descurtir"
+    } else {
+      // Descurtida
+      iconeLike.textContent = 'sentiment_neutral'; // Volta o ícone para o estado inicial
+      this.title = "Curtir"; // Volta o título do botão para "Curtir"
+      iconeLike.style.color = '#004aad';
+    }
+  });
+});
 
-        botaoClicado.innerHTML = `<span class="material-symbols-outlined">
-mood
-</span>`;
-        botaoClicado.style.color = "yellow";
 
+const modal = document.getElementById('modal-comentarios');
+const btnsComentario = document.querySelectorAll('.btn-coment');
 
-    })
-})
-
-
-let modal = document.getElementById('modal-comentarios');
-let btnsComentario = document.querySelectorAll('.btn-coment');
-
-btnsComentario.forEach(botao =>{
-    botao.addEventListener('click', function(e){
+btnsComentario.forEach(botaob =>{
+    botaob.addEventListener('click', function(e){
         let botaoClicado = e.target;
-
         modal.classList.toggle('ativo');
+
+        const fecharBtn = document.querySelector('.fechar-modal-comentario');
+
+        fecharBtn.addEventListener('click', ()=>{
+            modal.classList.remove('ativo');
+        })
     })
-})
-
-modal.addEventListener('click', ()=>{
-
 })
